6a9ee2dcacbacf8cb7ebb3618c90ff36208b0b5a
shanxu <kingundertree@163.com>
2014-06-26 22:25:13 +0800

    add pro

---
+++ BaseTabbarController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BaseTabbarController
+//
+//  Created by xiazer on 14-6-26.
+//  Copyright (c) 2014年 xiazer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BaseTabbarControllerTests/BaseTabbarControllerTests.m
@@ -0,0 +1,34 @@
+//
+//  BaseTabbarControllerTests.m
+//  BaseTabbarControllerTests
+//
+//  Created by xiazer on 14-6-26.
+//  Copyright (c) 2014年 xiazer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BaseTabbarControllerTests : XCTestCase
+
+@end
+
+@implementation BaseTabbarControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
